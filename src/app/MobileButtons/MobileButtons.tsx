5e2067595e5595a653f3a30bcde868f0d3d83dbc
Vito Medlej <vito.medlej@gmail.com>
2024-08-19 21:38:03 +0300

    flex

--- src/app/MobileButtons/MobileButtons.tsx
+++ src/app/MobileButtons/MobileButtons.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 const MobileButtons = ({isLoaded} : {isLoaded: boolean}) => {
   return (
 
-    <div className="mobile-buttons-container" style={{ display: isLoaded ? 'block' : 'none' }}>
+    <div className="mobile-buttons-container" style={{ display: isLoaded ? 'flex' : 'none' }}>
     <button id="w" className="mobile-button">W</button>
     <div className="horizontal-buttons">
       <button id="a" className="mobile-button">A</button>
